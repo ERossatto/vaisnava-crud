b49582fa2fc82ab1419a779457c6d9a8cd927f9e
Ettore Rossatto <ettore.rossatto@ciadaconsulta.com.br>
2021-05-12 19:07:44 -0300

    Delete tests

--- src/app/content/home/home.component.ts
+++ src/app/content/home/home.component.ts
@@ -17,26 +17,9 @@ export class HomeComponent implements OnInit {
   }
 
   teste1() {
-    console.log('1-->: ', this._iteratorExerciseService.getRandomNumberList());
   }
 
   teste2() {
-    const list = this._iteratorExerciseService.getRandomNumberList();
-
-    const listOfBiggerThanAHundred = list.filter( item => item > 100 );
-
-    console.log('2-->: ', this._iteratorExerciseService.checkBiggerThanAHundred( listOfBiggerThanAHundred ));
   }
 
-  teste3() {
-    const list = this._iteratorExerciseService.getRandomNumberList();
-
-    const listOfDividibleByTwo = list.filter( item => {
-      const restOfDivideByTwo = item % 2;
-      return (restOfDivideByTwo == 0);
-    })
-
-    console.log('3-->: ', this._iteratorExerciseService.checkDivisibleByTwo(listOfDividibleByTwo));
-
-  }
 }
--- src/app/shared/services/iterator-exercise.service.ts
+++ src/app/shared/services/iterator-exercise.service.ts
@@ -5,7 +5,6 @@ import { Injectable } from '@angular/core';
 })
 export class IteratorExerciseService {
 
-
   constructor() { }
 
   public getRandomNumberList(): number[] {
